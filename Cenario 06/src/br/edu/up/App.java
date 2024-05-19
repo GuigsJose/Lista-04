5023c3497efac0d0dbab324c4f43e134d3ab3bda
Guilherme Silva <guizaomtz@gmail.com>
2024-05-19 10:52:18 -0300

    Instanciando na main, e testando

--- Cenario 06/src/br/edu/up/App.java
+++ Cenario 06/src/br/edu/up/App.java
@@ -1,6 +1,12 @@
 package br.edu.up;
+
+import br.edu.up.Controller.AeroportoController;
+import br.edu.up.View.AeroportoView;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        AeroportoController controller = new AeroportoController();
+        AeroportoView view = new AeroportoView(controller);
+        view.mostrarMenu();
     }
 }
