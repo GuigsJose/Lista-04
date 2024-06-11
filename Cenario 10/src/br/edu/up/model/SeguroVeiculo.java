de28f04b83d04f0d27ca66ad3e08ad4d750f5a91
Guilherme Silva <guizaomtz@gmail.com>
2024-06-11 17:21:55 -0300

    Criação das models seguro vida e seguro veiculo

---
+++ Cenario 10/src/br/edu/up/model/SeguroVeiculo.java
@@ -0,0 +1,44 @@
+package br.edu.up.model;
+
+import java.time.LocalDate;
+
+public class SeguroVeiculo extends Seguro{
+    private double vlrFranquia;
+    private boolean temCarroReserva;
+    private boolean cobreVidros;
+    
+    public SeguroVeiculo(String apolice, Segurado segurado, double vlrApolice, LocalDate dataInicio, LocalDate dataFim,
+            double vlrFranquia, boolean temCarroReserva, boolean cobreVidros) {
+        super(apolice, segurado, vlrApolice, dataInicio, dataFim);
+        this.vlrFranquia = vlrFranquia;
+        this.temCarroReserva = temCarroReserva;
+        this.cobreVidros = cobreVidros;
+    }
+
+    public double getVlrFranquia() {
+        return vlrFranquia;
+    }
+
+    public void setVlrFranquia(double vlrFranquia) {
+        this.vlrFranquia = vlrFranquia;
+    }
+
+    public boolean isTemCarroReserva() {
+        return temCarroReserva;
+    }
+
+    public void setTemCarroReserva(boolean temCarroReserva) {
+        this.temCarroReserva = temCarroReserva;
+    }
+
+    public boolean isCobreVidros() {
+        return cobreVidros;
+    }
+
+    public void setCobreVidros(boolean cobreVidros) {
+        this.cobreVidros = cobreVidros;
+    }
+
+    
+    
+}
---
+++ Cenario 10/src/br/edu/up/model/SeguroVida.java
@@ -0,0 +1,33 @@
+package br.edu.up.model;
+
+import java.time.LocalDate;
+
+public class SeguroVida extends Seguro{
+    private boolean cobreDoenca;
+    private boolean cobreAcidente;
+    
+    public SeguroVida(String apolice, Segurado segurado, double vlrApolice, LocalDate dataInicio, LocalDate dataFim,
+            boolean cobreDoenca, boolean cobreAcidente) {
+        super(apolice, segurado, vlrApolice, dataInicio, dataFim);
+        this.cobreDoenca = cobreDoenca;
+        this.cobreAcidente = cobreAcidente;
+    }
+
+    public boolean isCobreDoenca() {
+        return cobreDoenca;
+    }
+
+    public void setCobreDoenca(boolean cobreDoenca) {
+        this.cobreDoenca = cobreDoenca;
+    }
+
+    public boolean isCobreAcidente() {
+        return cobreAcidente;
+    }
+
+    public void setCobreAcidente(boolean cobreAcidente) {
+        this.cobreAcidente = cobreAcidente;
+    }
+
+    
+}
