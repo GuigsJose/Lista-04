7b28a24fbc8c2b98c08920aedbdeccb87471bbf8
Guilherme Silva <guizaomtz@gmail.com>
2024-06-11 17:05:15 -0300

    implementado e testado

--- Cenario 09/src/br/edu/up/App.java
+++ Cenario 09/src/br/edu/up/App.java
@@ -1,5 +1,12 @@
+""
+
+import Controller.ClienteController;
+import View.PrincipalView;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ClienteController controller = new ClienteController();
+        PrincipalView view = new PrincipalView(controller);
+        view.mostrarMenu();
     }
 }
